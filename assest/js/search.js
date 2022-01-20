667341942d4bd873fc2156b93e2d3f95a5e4ee52
Thoryaa <93941131+Thoryaa@users.noreply.github.com>
2022-01-20 15:09:48 +0300

    increase cart and search

---
+++ assest/js/search.js
@@ -0,0 +1,30 @@
+function myFunction() {
+    // Declare variables
+    var input, filter, div, p, span, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    div = document.getElementById("product");
+    p = div.getElementsByTagName('p');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < p.length; i++) {
+        span = p[i].getElementsByTagName("span")[0];
+        txtValue = span.textContent || span.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            p[i].style.display = "";
+        } else {
+            p[i].style.display = "none";
+        }
+    }
+}
+/****************************************** */
+let coun = 0;
+document.getElementById('add-to-cart').addEventListener('click', () => {
+
+    sessionStorage.setItem('cart-count', ++coun);
+    document.getElementById('cart-coun').innerHTML = ++coun;
+
+});
+
+
+console.log(coun);
